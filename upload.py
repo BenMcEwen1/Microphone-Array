02bf6b7168ec44ab998ff22c1985431a7f9e6dfd
BenMcEwen1 <benmcewen@outlook.com>
2021-04-13 13:31:00 +1200

    Raspberry Pi Test

--- upload.py
+++ upload.py
@@ -7,13 +7,6 @@ url = 'https://api.cacophony.org.nz/api/v1/recordings/device/TEST/group/Audio Lu
 
 filename = "test.mp4"
 
-# payload = json.dumps({
-#     "data": {
-#         "type": 'audio'
-#     },
-#     "file": os.path.basename(filename)
-# })
-
 token = "JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAyLCJfdHlwZSI6InVzZXIiLCJpYXQiOjE2MDU0NzE1NTF9.fN1GTV2GIWn6ARkSPQnqPe8xCmgukfJjgh5em93lElM"
 props = {"type": "audio"}
 json_props = json.dumps(props)
