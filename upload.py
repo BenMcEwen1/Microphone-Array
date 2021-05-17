2f830c388459d6fcff23cf141bc2584496294eca
BenMcEwen1 <benmcewen@outlook.com>
2021-05-17 15:58:05 +1200

    Added duration

--- upload.py
+++ upload.py
@@ -8,18 +8,16 @@ from datetime import datetime
 current_date = datetime.now()
 print(current_date.isoformat())
 
-json_datetime = json.dumps(current_date.isoformat())
-
-props = {"type": "audio"}
-json_props = json.dumps(props)
-
 def upload_recording(filename, DOA):
     # Upload the recordings to the Cacophony server
-    json_DOA = json.dumps(DOA) 
+   
+    props = {"type": "audio", "duration": 60, "additionalMetaData": DOA, "recordingDateTime": current_date.isoformat()}
+
+    json_props = json.dumps(props)
 
     with open(filename, "rb") as content:
         multipart_data = MultipartEncoder(
-            fields = {"data": json_props, "file": (os.path.basename(filename), content), "additionalMetadata": json_DOA, "recordingDateTime": json_datetime}
+            fields = {"data": json_props, "file": (os.path.basename(filename), content)}
         )
         headers = {"Content-type": multipart_data.content_type, "Authorization": parameters.user_token}
         response = requests.request("POST", parameters.upload, headers=headers, data=multipart_data)
